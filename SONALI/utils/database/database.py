fc26f5135ca8960213c85ce4249ffeef1b239a02
ARJUN <pachangedinanath@gmail.com>
2025-12-26 22:44:28 +0530

    Update database.py

--- SONALI/utils/database/database.py
+++ SONALI/utils/database/database.py
@@ -795,4 +795,5 @@ async def remove_banned_user(user_id: int):
 
 async def get_private_served_chats() -> list:
     chats_list = []
-    async for chat in privatedb.find({"chat_id"})
+    async for chat in privatedb.find({"chat_id": chat_id})
+        
